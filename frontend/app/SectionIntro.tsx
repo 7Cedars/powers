582101799dce57af5400179f501733493a360ca4
7Cedars <cedars7@proton.me>
2025-03-21 10:43:01 +0000

    update SectionIntro component to use new home4 image; add home2, home3, and home4 images

--- frontend/app/SectionIntro.tsx
+++ frontend/app/SectionIntro.tsx
@@ -23,7 +23,7 @@ export function SectionIntro() {
           {/* Image  */}
           <section className = "grow h-full w-full flex flex-col justify-center items-center" style = {{position: 'relative', width: '100%', height: '100%'}}> 
             <Image 
-                src={"/home.png"} 
+                src={"/home4.png"} 
                 className = "p-2 rounded-md" 
                 style={{objectFit: "contain", objectPosition: "center"}}
                 fill={true}
