8a4df1bc30922795b7f7101024fbf532675db0ae
7Cedars <cedars7@proton.me>
2025-03-28 09:30:20 +0000

    landing page fixes

--- frontend/app/SectionIntro.tsx
+++ frontend/app/SectionIntro.tsx
@@ -8,7 +8,7 @@ import { ArrowUpRightIcon, ChevronDownIcon } from "@heroicons/react/24/outline";
 export function SectionIntro() { 
 
   return (    
-    <section id="intro" className="w-full min-h-[100vh] h-full max-h-fit flex flex-col gap-6 justify-between items-center bg-gradient-to-b from-indigo-700 to-blue-600 snap-start snap-always pt-4 px-6">
+    <section id="intro" className="w-full min-h-[100vh] h-full max-h-fit flex flex-col gap-6 justify-between items-center bg-gradient-to-b from-indigo-700 to-blue-600 snap-start snap-always py-12 px-6">
         {/* title  */}
           <section className="w-full min-h-fit flex flex-col justify-center items-center pt-12">
               <div className = "w-full flex flex-col justify-center items-center md:text-4xl text-3xl font-bold text-slate-100 max-w-4xl text-center text-pretty">
@@ -21,7 +21,7 @@ export function SectionIntro() {
 
           {/* sm:h-full sm:w-full sm:max-h-full sm:max-w-full max-h-0 max-w-0 */}
           {/* Image  */}
-          <section className = "grow h-full w-full flex flex-col justify-center items-center" style = {{position: 'relative', width: '100%', height: '100%'}}> 
+          <section className = "grow h-full w-full max-w-6xl flex flex-col justify-center items-center" style = {{position: 'relative', width: '100%', height: '100%'}}> 
             <Image 
                 src={"/home2.png"} 
                 className = "p-2 rounded-md" 
@@ -33,10 +33,16 @@ export function SectionIntro() {
           </section>
 
           {/* Bottom text */}
-          <section className="w-full min-h-fit flex justify-center items-center md:text-xl text-lg text-slate-100 max-w-2xl text-center">
+          <section className="w-full min-h-fit flex flex-col gap-2 justify-center items-center md:text-xl text-lg text-slate-100 max-w-4xl text-center">
+              <p>
                 Powers is a Proof of Concept of a role restricted governance protocol. 
+              </p>
+              <p>
                 It combines a governance engine with role restricted and modular contracts, called laws. 
                 Together they create a governance protocol that is more flexible, upgradable and safe than existing alternatives.
+              </p>
+  
+
           </section>
 
 
